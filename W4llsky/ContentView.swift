d83e993724871e87f88bef188578ef60687acaed
Antonio Orozco <orozco6453@gmail.com>
2026-08-22 21:11:08 -0700

    Initial Commit

---
+++ W4llsky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  W4llsky
+//
+//  Created by Antonio Orozco on 22/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ W4llsky/W4llskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  W4llskyApp.swift
+//  W4llsky
+//
+//  Created by Antonio Orozco on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct W4llskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
